charlesmunger/roboguice

commit c331337460408c0d1a54c121b105ae536ab9fe14

Add AccountManager binding

roboguice/src/main/java/roboguice/config/DefaultRoboModule.java
@@ -8,6 +8,7 @@ import roboguice.util.Ln;
 import roboguice.util.RoboAsyncTask;
 import roboguice.util.Strings;
 
+import android.accounts.AccountManager;
 import android.app.*;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -21,6 +22,7 @@ import android.location.LocationManager;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
+import android.os.Build.VERSION;
 import android.os.PowerManager;
 import android.os.Vibrator;
 import android.provider.Settings;
@@ -93,6 +95,15 @@ public class DefaultRoboModule extends AbstractModule {
         final EventListenerThreadingDecorator observerThreadingDecorator = new EventListenerThreadingDecorator();
         final String androidId = Secure.getString(application.getContentResolver(), Secure.ANDROID_ID);
 
+        // Package Info
+        try {
+            final PackageInfo info = application.getPackageManager().getPackageInfo(application.getPackageName(),0);
+            bind(PackageInfo.class).toInstance(info);
+        } catch( PackageManager.NameNotFoundException e ) {
+            throw new RuntimeException(e);
+        }
+
+
         if(Strings.notEmpty(androidId))
             bindConstant().annotatedWith(Names.named(Settings.Secure.ANDROID_ID)).to(androidId);
 
@@ -120,13 +131,6 @@ public class DefaultRoboModule extends AbstractModule {
         bind(EventListenerThreadingDecorator.class).toInstance(observerThreadingDecorator);
 
 
-        // Package Info
-        try {
-            final PackageInfo info = application.getPackageManager().getPackageInfo(application.getPackageName(),0);
-            bind(PackageInfo.class).toInstance(info);
-        } catch( PackageManager.NameNotFoundException e ) {
-            throw new RuntimeException(e);
-        }
 
         // System Services
         bind(LocationManager.class).toProvider(new SystemServiceProvider<LocationManager>(Context.LOCATION_SERVICE));
@@ -167,6 +171,13 @@ public class DefaultRoboModule extends AbstractModule {
         if(hasCompatibilityLibrarySupport) {
             bind(FragmentManager.class).toProvider(FragmentManagerProvider.class);
         }
+
+        // 2.0 Eclair
+        if( VERSION.SDK_INT>=5 ) {
+            bind(AccountManager.class).toProvider(AccountManagerProvider.class);
+        }
+
+
     }
 
 }

roboguice/src/main/java/roboguice/inject/AccountManagerProvider.java
@@ -1,0 +1,15 @@
+package roboguice.inject;
+
+import android.accounts.AccountManager;
+import android.content.Context;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+@ContextScoped
+public class AccountManagerProvider implements Provider<AccountManager> {
+    @Inject protected Context context;
+
+    public AccountManager get() {
+        return AccountManager.get(context);
+    }
+}

commit 040d267b5765c2cadee454b687fa02213c178655

Minimum SDK version bumped to 5
Support for API level 3 has been broken since
c331337460408c0d1a54c121b105ae536ab9fe14, and nobody noticed or cared
for the last 2 years. API level 5+ represents 99.9% of android devices,
so there's really no point in adding compatibility cruft to the code to
support <5.

Fixes #26.

roboguice/AndroidManifest.xml
@@ -7,9 +7,8 @@
       android:versionCode="1"
       android:versionName="1.0">
 
-    <uses-sdk android:minSdkVersion="3" />
+    <uses-sdk android:minSdkVersion="5" />
     
-    <application android:icon="@drawable/icon" android:label="@string/app_name" >
-    </application>
+    <application android:icon="@drawable/icon" android:label="@string/app_name" />
 
 </manifest>

roboguice/src/main/java/roboguice/config/DefaultRoboModule.java
@@ -1,6 +1,38 @@
 package roboguice.config;
 
+import android.accounts.AccountManager;
 import android.app.*;
+import roboguice.activity.RoboActivity;
+import roboguice.event.EventManager;
+import roboguice.event.ObservesTypeListener;
+import roboguice.event.eventListener.factory.EventListenerThreadingDecorator;
+import roboguice.inject.AccountManagerProvider;
+import roboguice.inject.AssetManagerProvider;
+import roboguice.inject.ContentResolverProvider;
+import roboguice.inject.ContextScope;
+import roboguice.inject.ContextScopedSystemServiceProvider;
+import roboguice.inject.ContextSingleton;
+import roboguice.inject.ExtrasListener;
+import roboguice.inject.FragmentManagerProvider;
+import roboguice.inject.HandlerProvider;
+import roboguice.inject.NullProvider;
+import roboguice.inject.PreferenceListener;
+import roboguice.inject.ResourceListener;
+import roboguice.inject.ResourcesProvider;
+import roboguice.inject.SharedPreferencesProvider;
+import roboguice.inject.SystemServiceProvider;
+import roboguice.inject.ViewListener;
+import roboguice.service.RoboService;
+import roboguice.util.Ln;
+import roboguice.util.Strings;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Key;
+import com.google.inject.Provider;
+import com.google.inject.TypeLiteral;
+import com.google.inject.matcher.Matchers;
+import com.google.inject.name.Names;
+
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.SharedPreferences;
@@ -13,7 +45,6 @@ import android.location.LocationManager;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.wifi.WifiManager;
-import android.os.Build.VERSION;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.Vibrator;
@@ -23,20 +54,6 @@ import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
-import com.google.inject.AbstractModule;
-import com.google.inject.Key;
-import com.google.inject.Provider;
-import com.google.inject.TypeLiteral;
-import com.google.inject.matcher.Matchers;
-import com.google.inject.name.Names;
-import roboguice.activity.RoboActivity;
-import roboguice.event.EventManager;
-import roboguice.event.ObservesTypeListener;
-import roboguice.event.eventListener.factory.EventListenerThreadingDecorator;
-import roboguice.inject.*;
-import roboguice.service.RoboService;
-import roboguice.util.Ln;
-import roboguice.util.Strings;
 
 /**
  * A Module that provides bindings and configuration to use Guice on Android.
@@ -55,8 +72,8 @@ import roboguice.util.Strings;
  * @author Mike Burton
  */
 public class DefaultRoboModule extends AbstractModule {
+    //Can't be genericized because doing so introduces a dependency on the support library
     protected static final Class fragmentManagerClass;
-    protected static final Class accountManagerClass;
 
     static {
         Class c = null;
@@ -65,15 +82,6 @@ public class DefaultRoboModule extends AbstractModule {
         } catch( Throwable ignored ) {}
         fragmentManagerClass = c;
     }
-
-    static {
-        Class c = null;
-        try {
-            c = Class.forName("android.accounts.AccountManager");
-        } catch( Throwable ignored ) {}
-        accountManagerClass = c;
-    }
-
 
     protected Application application;
     protected ContextScope contextScope;
@@ -187,13 +195,7 @@ public class DefaultRoboModule extends AbstractModule {
             bind(fragmentManagerClass).toProvider(FragmentManagerProvider.class);
         }
 
-
-        // 2.0 Eclair
-        if( VERSION.SDK_INT>=5 ) {
-            bind(accountManagerClass).toProvider(AccountManagerProvider.class);
-        }
-
-
+        bind(AccountManager.class).toProvider(AccountManagerProvider.class);
     }
 
 }